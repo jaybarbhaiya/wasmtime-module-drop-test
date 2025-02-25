96f4c2972e86cf56670f2fac15c863026c473a11
Jay Barbhaiya <jay.barbhaiya@sap.com>
2025-02-25 08:42:58 +0100

    disable Config::native_unwind_info

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
-use wasmtime::{Engine, Module};
+use wasmtime::{Config, Engine, Module};
 
 const WASM: &[u8] = include_bytes!("../rust_wasm_module.wasm");
 
 pub fn create_module() -> Module {
-    let engine = Engine::default();
+    let mut config = Config::new();
+    config.native_unwind_info(false);
+    let engine = Engine::new(&config).expect("failed to create engine");
     Module::new(&engine, WASM).expect("failed to create module")
 }
